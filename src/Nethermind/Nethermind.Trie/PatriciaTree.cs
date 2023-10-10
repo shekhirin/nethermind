8ef73d9146a4f5d6bd19868512904f683ca3b88b
Amirul Ashraf <asdacap@gmail.com>
2023-10-10 12:49:38 +0800

    Some adjustment

--- src/Nethermind/Nethermind.Trie/PatriciaTree.cs
+++ src/Nethermind/Nethermind.Trie/PatriciaTree.cs
@@ -1138,7 +1138,7 @@ namespace Nethermind.Trie
             BatchedTrieVisitor<TreeLeafContext> batchedTrieVisitor = new(
                 visitorAdapter, resolver, new VisitingOptions()
                 {
-                    FullScanMemoryBudget = 16.GiB(),
+                    FullScanMemoryBudget = 32.GiB(),
                 }, 80);
             batchedTrieVisitor.Start(rootHash, new TreeLeafContext()
             {
